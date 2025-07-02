3508fa915c2587e94c47fa0020592ecd8b16ac44
uvanp-08 <uvanp05@gmail.com>
2025-07-02 22:28:05 +0530

    Twelth commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { createUser, getAllUsers, findUserByEmail } = require("../models/userModel");
 
